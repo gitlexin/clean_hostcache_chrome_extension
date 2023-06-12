b8be8be17ed505e67536b0cb071af8a2b116c8cc
屈富林 <772821140@qq.com>
2023-06-12 17:29:32 +0800

    优化交互

--- background.js
+++ background.js
@@ -1,45 +1,84 @@
-chrome.runtime.onMessage.addListener(async function (message, sender, sendResponse) {
-    function domClick(domId) {
-        document.getElementById(domId).click();
-    }
-    async function getCurrentTab() {
-        let queryOptions = {
-            active: true,
-            lastFocusedWindow: true
-        };
-        let [tab] = await chrome.tabs.query(queryOptions);
-        return tab;
-    }
-    if (message.action === "clear") {
+chrome.action.onClicked.addListener(async () => {
+    try {
+        // Get the current tab id
         let currentTab = await getCurrentTab();
-        let newTab;
+        let currentTabId = currentTab.id;
 
-        newTab = await chrome.tabs.create({
-            url: "chrome://net-internals/#dns"
-        });
-        await chrome.scripting.executeScript({
-            target: {
-                tabId: newTab.id
-            },
-            func: domClick,
-            args: ["dns-view-clear-cache"]
-        })
-        await chrome.tabs.remove(newTab.id)
+        // Create a new tab and open chrome://net-internals/#dns
+        let dnsTab = await createTab("chrome://net-internals/#dns");
 
-        newTab = await chrome.tabs.create({
-            url: "chrome://net-internals/#sockets"
-        });
+        // Click the button with id dns-view-clear-cache
+        await clickButton(dnsTab.id, "dns-view-clear-cache");
 
-        await chrome.scripting.executeScript({
-            target: {
-                tabId: newTab.id
-            },
-            func: domClick,
-            args: ["sockets-view-flush-button"]
-        })
-        await chrome.tabs.remove(newTab.id)
+        // Close the tab
+        await closeTab(dnsTab.id);
 
-        await chrome.tabs.update(currentTab.id, {active: true});
-        await chrome.tabs.reload(currentTab.id);
+        // Create a new tab and open chrome://net-internals/#sockets
+        let socketsTab = await createTab("chrome://net-internals/#sockets");
+
+        // Click the button with id sockets-view-flush-button
+        await clickButton(socketsTab.id, "sockets-view-flush-button");
+
+        // Close the tab
+        await closeTab(socketsTab.id);
+
+        // Switch back to the original tab
+        await switchToTab(currentTabId);
+        await chrome.tabs.reload(currentTabId);
+    } catch (error) {
+        console.error(error);
     }
 });
+
+// Helper functions
+
+// Get the current active tab
+async function getCurrentTab() {
+    let tabs = await chrome.tabs.query({ active: true });
+    if (tabs.length > 0) {
+        return tabs[0];
+    } else {
+        throw new Error("No active tab found");
+    }
+}
+
+// Create a new tab with a given url
+async function createTab(url) {
+    let tab = await chrome.tabs.create({ url: url });
+    if (tab) {
+        return tab;
+    } else {
+        throw new Error("Failed to create tab");
+    }
+}
+
+// Click a button with a given id in a tab
+async function clickButton(tabId, buttonId) {
+    await chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        func: (buttonId) => {
+            let button = document.getElementById(buttonId);
+            if (button) {
+                button.click();
+            } else {
+                throw new Error("Button not found");
+            }
+        },
+        args: [buttonId],
+    });
+}
+
+// Close a tab with a given id
+async function closeTab(tabId) {
+    await chrome.tabs.remove(tabId);
+}
+
+// Switch to a tab with a given id
+async function switchToTab(tabId) {
+    let tab = await chrome.tabs.update(tabId, { active: true });
+    if (tab) {
+        return tab;
+    } else {
+        throw new Error("Failed to switch to tab");
+    }
+}
--- popup.js
+++
@@ -1,8 +0,0 @@
-// 获取按钮元素
-var button = document.getElementById("clear");
-
-// 给按钮添加点击事件监听器
-button.addEventListener("click", function() {
-  // 发送消息给后台服务工作线程
-  chrome.runtime.sendMessage({action: "clear"});
-});
